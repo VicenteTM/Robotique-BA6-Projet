56dbc444a8170dc29aa34c23dd13d62e4ca210b2
VicenteTM <vicente.cacrbon@epfl.ch>
2022-05-05 16:14:56 +0200

    Fix merge

--- Projet_Movo_bot/Movo_bot/main.c
+++ Projet_Movo_bot/Movo_bot/main.c
@@ -34,23 +34,6 @@ static void serial_start(void)
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
-static void timer12_start(void)
-{
-    //General Purpose Timer configuration   
-    //timer 12 is a 16 bit timer so we can measure time
-    //to about 65ms with a 1Mhz counter
-    static const GPTConfig gpt12cfg = {
-        1000000,        /* 1MHz timer clock in order to measure uS.*/
-        NULL,           /* Timer callback.*/
-        0,
-        0
-    };
-
-    gptStart(&GPTD12, &gpt12cfg);
-    //let the timer count to max value
-    gptStartContinuous(&GPTD12, 0xFFFF);
-}
-
 int main(void)
 {
     halInit();
